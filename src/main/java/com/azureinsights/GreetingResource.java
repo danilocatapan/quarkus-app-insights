ce72cc1395b079e81e313f71aa0ade8e8c0ca420
Danilo <danilo.foton@foton.la>
2021-09-13 21:48:28 -0300

    feat: add TelemetryClient to use on GreetingResource

--- src/main/java/com/azureinsights/GreetingResource.java
+++ src/main/java/com/azureinsights/GreetingResource.java
@@ -1,11 +1,15 @@
 package com.azureinsights;
 
+import com.microsoft.applicationinsights.TelemetryClient;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Path("/greeting")
@@ -14,11 +18,29 @@ public class GreetingResource {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    static final TelemetryClient telemetryClient = new TelemetryClient();
+
     @GET
     @Path("/{name}")
     @Produces(MediaType.TEXT_PLAIN)
     public Response greeting(@PathParam("name") String name) {
+
+        Map<String, String> properties = new HashMap<>();
+        properties.put("NAME", name);
+
+        Map<String, Double> metrics = new HashMap<>();
+        metrics.put("metrics", 10.0);
+
+        telemetryClient.trackEvent("INIT GREETING REQUEST");
+        telemetryClient.trackEvent("GREETING PROPERTIES AND METRICS", properties, metrics);
+
         Greeting greeting = new Greeting(counter.incrementAndGet(), String.format(template, name));
-        return Response.ok(greeting.toString()  ).build();
+
+        telemetryClient.trackEvent("MOUNTED GREETING REQUEST");
+
+        String hello = greeting.toString();
+        telemetryClient.trackEvent("FORMATTED HELLO: " + hello);
+
+        return Response.ok(hello).build();
     }
 }
